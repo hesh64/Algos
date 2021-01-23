826337b9895ff105dac82164f8ec73bd009a39be
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-01-22 20:37:20 -0800

    sliding window

--- algos/sliding window/repeat.substring.py
+++ algos/sliding window/repeat.substring.py
@@ -18,6 +18,61 @@ def longest_repeat(string):
     return max_length
 
 
+'''
+Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’
+letters with any letter, find the length of the longest substring having the same letters after
+replacement.
+'''
+
+
+# todo come back to this.
+def max_length_replace_k(string, k):
+    start, max_length, max_repeat_letter_count = 0, 0, 0
+    frequency_map = {}
+
+    for end in range(len(string)):
+        if string[end] not in frequency_map:
+            frequency_map[string[end]] = 0
+        frequency_map[string[end]] += 1
+
+        max_repeat_letter_count = max(max_repeat_letter_count, frequency_map[string[end]])
+
+        if (end - start + 1 - max_repeat_letter_count) > k:
+            frequency_map[string[string]] -= 1
+            start += 1
+
+        max_length = max(max_length, end - start + 1)
+
+    return max_length
+
+
+'''
+Given an array containing 0s and 1s, if you are allowed 
+to replace no more than ‘k’ 0s with 1s, find the length of 
+the longest contiguous subarray having all 1s.
+'''
+
+
+# O(n + n) -> O(n)
+def longest_zeros_and_ones(array, k):
+    start = 0
+    max_1s = 0
+    zeros = 0
+
+    for end in range(len(array)):
+        if array[end] == 0:
+            zeros += 1
+
+        while zeros > k:
+            if array[start] == 0:
+                zeros -= 1
+            start += 1
+
+        max_1s = max(end - start + 1, max_1s)
+
+    return max_1s
+
+
 def main():
     result = longest_repeat('aabccbb')
     print('longest_repeat', result)
@@ -27,6 +82,13 @@ def main():
 
     result = longest_repeat('abccde')
     print('longest_repeat', result)
+
+    # this works wow...
+    result = longest_zeros_and_ones([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2)
+    print(result)
+
+    result = longest_zeros_and_ones([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], k=3)
+    print(result)
 
 
 main()
