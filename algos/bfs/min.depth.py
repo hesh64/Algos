679bca2f5d1de479d176e388fdcc71abd43d52df
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-01-25 12:33:32 -0800

    connected

--- algos/bfs/min.depth.py
+++ algos/bfs/min.depth.py
@@ -75,6 +75,48 @@ def binary_tree_level_order_traversal_level_order_traversal(node, val):
     return None
 
 
+"""
+Problem Statement #
+Given a binary tree, connect each node with its level order successor. 
+The last node of each level should point to a null node.
+
+
+"""
+
+
+def binary_tree_level_order_traversal_level_order_traversal_connected(node):
+    nodes = []
+
+    if not node:
+        return nodes
+
+    queue = deque()
+    queue.append(node)
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        last_node = None
+        for _ in range(level_size):
+            curr_node = queue.popleft()
+            if curr_node.left:
+                queue.append(curr_node.left)
+
+            if curr_node.right:
+                queue.append(curr_node.right)
+
+            if last_node:
+                last_node.right = curr_node
+
+            last_node = curr_node
+
+            current_level.append(curr_node)
+        if last_node:
+            last_node.right = None
+        nodes.append(current_level)
+
+    return nodes
+
+
 def main():
     root = Node(1)
     root.left = Node(2)
@@ -95,6 +137,23 @@ def main():
 
     min_level = binary_tree_level_order_traversal_level_order_traversal(root, 6)
     print('minimum level is:', min_level)
+
+    root2 = Node(1)
+    root2.left = Node(2)
+    root2.right = Node(3)
+    root2.left.left = Node(4)
+    root2.left.right = Node(5)
+    root2.right.left = Node(6)
+    root2.right.right = Node(7)
+
+    head_connected = binary_tree_level_order_traversal_level_order_traversal_connected(root)
+    print(head_connected)
+    for i in range(len(head_connected)):
+        head = head_connected[i][0]
+        while head:
+            print(head.value)
+            head = head.right
+        print('end of linked list...')
 
 
 main()
