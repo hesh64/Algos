bba1ac1a4b332e346ccbfc462c445af0e472a2d6
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-02-06 19:03:47 -0800

    more

--- structs/trie/what.is.it.py
+++ structs/trie/what.is.it.py
@@ -15,11 +15,85 @@ Contact searching
 """
 
 
-class TrieNOde:
+class TrieNode:
     def __init__(self, char=''):
         self.char = char
         # depending on the size of the alphabet
         self.children = [None] * 26
         self.is_end_word = False
 
-    # def mark_as_leaf(self):
+    def mark_as_leaf(self):
+        self.is_end_word = True
+
+    def unmark_as_leaf(self):
+        self.is_end_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def get_index(self, t):
+        """helper to get the index of a letter"""
+        return ord(t) - ord('a')
+
+    def insert(self, key):
+        if key is None:
+            return
+
+        key = key.lower()
+        idx = 0
+        cur = self.root
+
+        while idx < len(key):
+            letter_index = self.get_index(key[idx])
+            if cur.children[letter_index] is None:
+                cur.children[letter_index] = TrieNode(key[idx])
+
+            cur = cur.children[letter_index]
+            idx += 1
+
+        cur.mark_as_leaf()
+        return True
+
+    def search(self, key):
+        if key is None:
+            return False
+
+        key = key.lower()
+        cur = self.root
+        idx = 0
+
+        while idx < len(key):
+            letter_index = self.get_index(key[idx])
+
+            if cur.children[letter_index] is None:
+                return False
+
+            cur = cur.children[letter_index]
+            idx += 1
+
+        return cur.is_end_word
+
+    def has_no_children(self, node):
+        for child in node.children:
+            if child is not None:
+                return False
+
+        return True
+
+    # def delete(self, key):
+    #     def del_helper(cur, key, idx=0):
+    #         if idx < len(key):
+    #             if self.get_index(key[idx]) in cur.children:
+    #                 del_helper(cur[self.get_index(key[idx])], key, idx + 1)
+    #                 if
+
+
+def main():
+    trie = Trie()
+    trie.insert('able')
+    print(trie.search('able'))
+
+
+main()
