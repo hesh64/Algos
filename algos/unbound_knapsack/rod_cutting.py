909f8f9fa8b5012c897f42e39cf5555ddc8feaf8
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-02-27 15:04:50 -0800

    rods

--- algos/unbound_knapsack/rod_cutting.py
+++ algos/unbound_knapsack/rod_cutting.py
@@ -44,12 +44,70 @@ def rot_cutting_rec(lengths, prices, length, index, profits):
     return max(profit1, profit2)
 
 
+def rod_cutting_memo(p, ls, l):
+    if len(p) == 0 or len(p) != len(ls) or l <= 0:
+        return 0
+
+    dp = [[-1 for _ in range(l + 1)] for _ in range(len(p))]
+    return rod_cut_memo(dp, p, ls, l, 0)
+
+
+def rod_cut_memo(dp, p, ls, l, i):
+    if l <= 0 or i >= len(p):
+        return 0
+
+    if dp[i][l] == -1:
+        p1 = 0
+        if ls[i] <= l:
+            p1 = p[i] + rod_cut_memo(dp, p, ls, l - ls[i], i)
+        dp[i][l] = max(p1, rod_cut_memo(dp, p, ls, l, i + 1))
+    return dp[i][l]
+
+
+def rod_cut_tabul(p, ls, l):
+    if len(p) != len(ls) or len(ls) == 0 or l <= 0:
+        return 0
+    n = len(p)
+    dp = [[0 for _ in range(l + 1)] for _ in range(n)]
+
+    for i in range(n):
+        dp[i][0] = 0
+
+    for j in range(1, l + 1):
+        if j % ls[0] == 0:
+            dp[0][j] = (j // ls[0]) * p[0]
+
+    for i in range(1, n):
+        for j in range(1, l + 1):
+            p1, p2 = 0, 0
+            if ls[i] <= j:
+                p1 = p[i] + dp[i][j - ls[i]]
+            if i > 0:
+                p2 = dp[i - 1][j]
+            dp[i][j] = max(p1, p2)
+
+    for r in dp:
+        print(r)
+    return dp[n - 1][l]
+
 def main():
     lengths = [1, 2, 3, 4, 5]
     prices = [2, 6, 7, 10, 13]
     length = 5
 
     result = rod_cutting(lengths, prices, length)
+    print(result)
+
+    lengths = [1, 2, 3, 4, 5]
+    prices = [2, 6, 7, 10, 13]
+    length = 5
+    result = rod_cutting_memo(prices, lengths, length)
+    print(result)
+
+    lengths = [1, 2, 3, 4, 5]
+    prices = [2, 6, 7, 10, 13]
+    length = 5
+    result = rod_cut_tabul(prices, lengths, length)
     print(result)
 
 
--- algos/unbound_knapsack/ubounded_knapsack.py
+++ algos/unbound_knapsack/ubounded_knapsack.py
@@ -2,19 +2,68 @@ def knapsack(p, w, c):
     if len(p) == 0:
         return 0
 
-    return knapsack_rec(p, w, c, 0, 0)
+    return knapsack_rec(p, w, c, 0)
 
 
-def knapsack_rec(p, w, c, i, profit):
-    if c <= 0 or i >= len(p):
-        return profit
+def knapsack_rec(p, w, c, i):
+    if c == 0 or i >= len(p):
+        return 0
 
     profit1 = 0
     if w[i] <= c:
-        profit1 = max(profit1, knapsack_rec(p, w, c - w[i], i, profit + p[i]))
+        profit1 = p[i] + knapsack_rec(p, w, c - w[i], i)
 
-    profit2 = knapsack_rec(p, w, c, i + 1, profit)
+    profit2 = knapsack_rec(p, w, c, i + 1)
+
     return max(profit1, profit2)
+
+
+def knapsack_memo(p, w, c):
+    if len(p) == 0 or c <= 0:
+        return 0
+
+    dp = [[-1 for _ in range(c + 1)] for _ in range(len(p))]
+    return knapsack_memo_rec(dp, p, w, c, 0)
+
+
+def knapsack_memo_rec(dp, p, w, c, i):
+    if c <= 0 or len(p) <= i:
+        return 0
+
+    if dp[i][c] == -1:
+        profit1 = 0
+        if w[i] <= c:
+            profit1 = p[i] + knapsack_memo_rec(dp, p, w, c - w[i], i)
+
+        profit2 = knapsack_memo_rec(dp, p, w, c, i + 1)
+        dp[i][c] = max(profit1, profit2)
+
+    return dp[i][c]
+
+
+def knapsack_tabul(p, w, c):
+    if len(p) == 0 or c <= 0 or len(w) != len(p):
+        return 0
+
+    n = len(p)
+    dp = [[0 for _ in range(c + 1)] for _ in range(n)]
+
+    for i in range(n):
+        dp[i][0] = 0
+
+    for i in range(n):
+        for j in range(1, c + 1):
+            profit1, profit2 = 0, 0
+            if w[i] <= j:
+                profit1 = p[i] + dp[i][j - w[i]]
+            if i > 0:
+                profit2 = dp[i - 1][j]
+            dp[i][j] = max(profit1, profit2)
+
+    for r in dp:
+        print(r)
+
+    return dp[n - 1][c]
 
 
 def main():
@@ -22,6 +71,18 @@ def main():
     w = [1, 2, 3]
     c = 5
     result = knapsack(p, w, c)
+    print(result)
+
+    p = [15, 20, 50]
+    w = [1, 2, 3]
+    c = 5
+    result = knapsack_memo(p, w, c)
+    print(result)
+
+    p = [15, 50, 60, 90]
+    w = [1, 3, 4, 5]
+    c = 8
+    result = knapsack_tabul(p, w, c)
     print(result)
 
 
