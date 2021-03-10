011b96780a0b307b5f341835a41240fd3d08d95b
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-03-09 20:59:20 -0800

    thats rad -- yeah i say rad now cause i am legit

--- sysdesign/network/maximum_routers.py
+++ sysdesign/network/maximum_routers.py
@@ -1,3 +1,4 @@
+# O(m * n)
 def maximum_router(m):
     dp = [[0 for i in range(len(m[0]))] for j in range(len(m))]
 
---
+++ sysdesign/network/update_vlan_id.py
@@ -0,0 +1,31 @@
+def update_ids(m, start, old_id, new_id):
+    visited = [[False for i in range(len(m[0]))] for j in range(len(m))]
+
+    def dfs(matrix, pos, visited, old_id, new_id):
+        row, col = pos
+        if 0 <= row < len(matrix) and 0 <= col < len(matrix[0]) \
+                and visited[row][col] is False and matrix[row][col] == old_id:
+            visited[row][col] = True
+            dfs(matrix, (row + 1, col), visited, old_id, new_id)
+            dfs(matrix, (row - 1, col), visited, old_id, new_id)
+            dfs(matrix, (row, col + 1), visited, old_id, new_id)
+            dfs(matrix, (row, col - 1), visited, old_id, new_id)
+            matrix[row][col] = 2
+
+    dfs(m, start, visited, old_id, new_id)
+
+
+def main():
+    matrix = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0],
+        [1, 1, 1, 0, 1],
+        [1, 1, 0, 1, 0],
+        [1, 1, 0, 0, 1],
+    ]
+    update_ids(matrix, (2, 2), 1, 2)
+    for row in matrix:
+        print(row)
+
+
+main()
