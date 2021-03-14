1bd0af123a5afd28cc1f5d7feb7d546201f2781b
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-03-13 19:16:41 -0800

    big yeet

--- sysdesign/uber/rain_water.py
+++ sysdesign/uber/rain_water.py
@@ -1,16 +1,52 @@
-# height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-height = [4, 2, 0, 3, 2, 5]
+class Stack:
+    def __init__(self):
+        self.right = []
 
-h = []
-n = 0
-left, right = height[0], height[2]
-for i in range(1, len(height) - 1):
+    def push(self, val):
+        self.right.append(val)
 
-    left = max(left, height[i - 1])
-    right = max(right, height[i + 1])
+    def pop(self):
+        return self.right.pop()
 
-    around = min(left, right)
-    if around > height[i]:
-        n += around - height[i]
+    def __len__(self):
+        return len(self.right)
 
-print(n)
+    def is_empty(self):
+        return len(self) == 0
+
+    def peak(self):
+        return self.right[-1]
+
+    def __repr__(self):
+        return str(self.right)
+
+
+def trap_water(elevation_map):
+    stack = Stack()
+
+    for i in reversed(elevation_map[1:]):
+        if len(stack) == 0 or stack.peak() <= i:
+            stack.push(i)
+        else:
+            stack.push(stack.peak())
+
+    left = elevation_map[0]
+    results = [0]
+    for i in range(1, len(elevation_map)):
+        left = max(elevation_map[i - 1], left)
+        min_h = min(left, stack.pop())
+        if elevation_map[i] < min_h:
+            results.append(min_h - elevation_map[i])
+        else:
+            results.append(0)
+
+    return sum(results)
+
+
+def main():
+    print(trap_water([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+    print(trap_water([4, 2, 0, 3, 2, 5]))
+    print(trap_water([1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3]))
+
+
+main()
