9d4b0a36b45299c702e05c8e121c479d05d62e2a
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-03-28 13:37:54 -0700

    dags dude

--- clrs/sinlge_source_shortest_path.py
+++ clrs/sinlge_source_shortest_path.py
@@ -31,10 +31,10 @@ def init_graph(g, s):
     return properties
 
 
-def relax(u, v, w):
-    if v['d'] > u['d'] + w:
-        v['d'] = u['d'] + w
-        v['p'] = u
+def relax(gp, u, v, w):
+    if gp[v]['d'] > gp[u]['d'] + w:
+        gp[v]['d'] = gp[u]['d'] + w
+        gp[v]['p'] = u
 
 
 # O(V * E)
@@ -46,7 +46,7 @@ def bellman_ford(g: Graph, s: int):
     for _ in range(g.size - 1):
         for u in g.vertices:
             for v in g.vertices[u]:
-                relax(gp[u], gp[v], g.get_weight(u, v))
+                relax(gp, u, v, g.get_weight(u, v))
 
     # O(E)
     for u in g.vertices:
@@ -59,12 +59,56 @@ def bellman_ford(g: Graph, s: int):
     return True
 
 
+def topological_sort(g: Graph):
+    in_degrees = {i: 0 for i in range(1, g.size + 1)}
+    for u in g.vertices:
+        for v in g.vertices[u]:
+            in_degrees[v] += 1
+
+    # print(in_degrees)
+
+    mothers = []
+    for k in in_degrees:
+        if in_degrees[k] == 0:
+            mothers.append(k)
+
+    ordered = []
+    while mothers:
+        u = mothers.pop(0)
+        ordered.append(u)
+
+        for v in g.vertices[u]:
+            in_degrees[v] -= 1
+            if in_degrees[v] == 0:
+                mothers.append(v)
+
+    return ordered
+
+
+# O(V + E)
+def shortest_paths_in_dag(g: Graph, s):
+    ordered = topological_sort(g)
+    gp = init_graph(g, s)
+    for u in ordered:
+        for v in g.vertices[u]:
+            relax(gp, u, v, g.get_weight(u, v))
+
+    return [[v['p'], k, v['d']] for k, v in gp.items() if v['p'] is not None]
+
+
 def main():
     edges = [[1, 2, 6], [1, 5, 7], [2, 3, 5], [2, 5, 8], [2, 4, -4], [3, 2, -2], [4, 1, 2], [4, 3, 7], [5, 4, 9],
              [5, 3, -3]]
 
     g = Graph(5, edges)
     shortest = bellman_ford(g, 1)
+    print(shortest)
+
+    # 6 verts
+    edges = [[1, 2, 5], [1, 3, 3], [2, 3, 2], [2, 4, 6], [3, 4, 7], [3, 5, 4], [3, 6, 2], [4, 5, -1], [4, 6, 1],
+             [5, 6, -2]]
+    g = Graph(6, edges)
+    shortest = shortest_paths_in_dag(g, 2)
     print(shortest)
 
 
