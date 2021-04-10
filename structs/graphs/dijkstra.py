a98d26406f3c0f54884968d990c410670ae9de61
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-04-10 14:42:13 -0700

    cleaned up

--- structs/graphs/dijkstra.py
+++ structs/graphs/dijkstra.py
@@ -37,13 +37,13 @@ def dijkstra(g: Graph, s):
     explored = set()
     n = g.vertices
 
-    preprocessing = {s: Vert(s, 0)}
+    s_vert = Vert(s, 0)
+    preprocessing = {s: s_vert}
+    h = [preprocessing[s]]
     for i in range(1, n + 1):
         if i != s:
             preprocessing[i] = Vert(i)
-
-    h = list(preprocessing.values())
-    heapify(h)
+            heappush(h, preprocessing[i])
 
     while h:
         u = heappop(h)
@@ -52,12 +52,26 @@ def dijkstra(g: Graph, s):
             if v_id not in explored:
                 v = preprocessing[v_id]
                 relax(u, v, g.get_cost(u.key, v.key))
+        # this is expensive v lg v
+        # an alternative is to heapify just the elements that were relax using a decrease key.
+        # this would require that we have access to the actual indices of the vertices. OR for
+        # a faster implementation use fib heaps
         heapify(h)
 
     return preprocessing
 
 
 def main():
+    g = Graph(6)
+    edges = [[1, 2, 1], [1, 4, 3], [2, 3, 2], [2, 4, 1], [3, 5, 8], [4, 5, 2], [4, 6, 4], [6, 5, 1]]
+    for u, v, c in edges:
+        g.add_edge(u, v, c)
+
+    g = dijkstra(g, 1)
+    for k in g:
+        print(g[k])
+    print('\n')
+
     g = Graph(6)
     edges = [[1, 2, 1], [1, 4, 3], [2, 3, 2], [2, 4, 1], [3, 5, 8], [4, 5, 2], [4, 6, 4], [6, 5, 1], [1, 6, 1]]
     for u, v, c in edges:
@@ -66,6 +80,17 @@ def main():
     g = dijkstra(g, 1)
     for k in g:
         print(g[k])
+    print('\n')
+
+    g = Graph(4)
+    edges = [[1, 2, 1], [2, 4, 6], [1, 3, 4], [3, 4, 3], [2, 3, 2]]
+    for u, v, c in edges:
+        g.add_edge(u, v, c)
+
+    g = dijkstra(g, 1)
+    for k in g:
+        print(g[k])
+    print('\n')
 
 
 main()
