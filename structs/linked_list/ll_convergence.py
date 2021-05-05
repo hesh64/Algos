edac44518cc2e078d3ed926d0d2db0d5b66fb6f1
hesh64 <59433056+hesh64@users.noreply.github.com>
2021-05-04 20:59:03 -0700

    pivots

--- structs/linked_list/ll_convergence.py
+++ structs/linked_list/ll_convergence.py
@@ -160,3 +160,96 @@ def find_converged_node_in_cyclic(l1, l2):
 
 
 print(find_converged_node_in_cyclic(l1, l2))
+
+
+# O(1)
+def delete_given_node(node):
+    node.data = node.next.data
+    node.next = node.next.next
+
+
+head = Node(1, Node(2, Node(3, Node(4))))
+delete_given_node(head.next.next)
+print(177)
+print(print_list(head))
+
+head = Node(2, Node(2, Node(3, Node(5, Node(7, Node(11, Node(11)))))))
+print(print_list(head))
+
+
+def remove_duplicates(head):
+    dum, cur = None, head
+    while cur:
+        dum, cur = cur, cur.next
+        if cur is not None and dum.data == cur.data:
+            dum.next = cur.next
+    return head
+
+
+remove_duplicates(head)
+print(print_list(head))
+
+head = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6))))))
+print(print_list(head))
+
+
+def even_odd_split(l1):
+    even, odd = None, None
+    even_head, odd_head = odd, even
+
+    while l1:
+        if l1.data % 2 == 0:
+            if even is None:
+                even_head = even = l1
+            else:
+                even.next = l1
+                even = even.next
+
+        else:
+            if odd is None:
+                odd_head = odd = l1
+            else:
+                odd.next = l1
+                odd = odd.next
+        l1 = l1.next
+
+    odd.next = None
+    even.next = odd_head
+    return even_head
+
+
+h = even_odd_split(head)
+print(print_list(h))
+
+
+def pivot_around(l, data):
+    d1 = d2 = d3 = dh1 = dh2 = dh3 = None
+
+    while l:
+        if l.data < data:
+            if d1 is None:
+                d1 = dh1 = l
+            else:
+                d1.next = l
+                d1 = d1.next
+        elif l.data == data:
+            if d2 is None:
+                d2 = dh2 = l
+            else:
+                d2.next = l
+                d2 = d2.next
+        elif l.data > data:
+            if d3 is None:
+                d3 = dh3 = l
+            else:
+                d3.next = l
+                d3 = d3.next
+        l = l.next
+
+    d1.next = dh2
+    d2.next = dh3
+    return dh1
+
+
+head = Node(3, Node(5, Node(1, Node(4, Node(2, Node(6, Node(9, Node(8, Node(7)))))))))
+print(print_list(pivot_around(head, 3)))
