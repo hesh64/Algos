6a2ca159a9b5ca6c660747ec4332e857737ae6ad
hesh64 <salim.hanhan19@gmail.com>
2021-06-06 18:47:51 -0700

    pain

--- data_structures/combinatorials/backtracking.py
+++ data_structures/combinatorials/backtracking.py
@@ -8,6 +8,7 @@ def backtrack(a, k, input,
     finished = False
 
     def helper(a, k, input):
+        nonlocal finished
         """
 
         :param a: a partial solution
@@ -16,7 +17,8 @@ def backtrack(a, k, input,
         :return:
         """
         if is_a_solution(a, k, input):
-            process_solution(a, k, input)
+            if process_solution(a, k, input):
+                finished = True
         else:
             k += 1
 
@@ -103,15 +105,13 @@ GraphForPath = namedtuple('GraphForPath', ('s', 't', 'g'))
 
 
 def all_paths_in_a_graph(input):
-    paths = 0
+    paths = []
 
     def is_a_solution(a, k, input):
         return a[k] == input.t
 
     def process_solution(a, k, input):
-        nonlocal paths
-        paths += 1
-        print('process_solution', a[:k + 1])
+        paths.append(a[:k + 1])
 
     def construct_candidates(a, k, input):
         in_path = [False] * (input.g.nvertices + 1)
@@ -141,7 +141,7 @@ def all_paths_in_a_graph(input):
               construct_candidates=construct_candidates,
               )
 
-    return paths
+    return len(paths), paths
 
 
 g = Graph(6)
@@ -150,3 +150,108 @@ for u, v in [[1, 2], [1, 3], [1, 4], [1, 5], [2, 6], [3, 6], [4, 6], [5, 6], [3,
 
 paths = all_paths_in_a_graph(GraphForPath(1, 3, g))
 print(paths)
+
+print('\n\n')
+Point = namedtuple('Point', ('x', 'y'))
+
+
+class Board:
+    def __init__(self, prepopulate):
+        self.m = [[None for _ in range(10)] for _ in range(10)]
+        self.freecount = 9 * 9
+        self.move = [None] * ((9 * 9) + 1)
+
+        for x, y, v in prepopulate:
+            self.m[x][y] = v
+            self.freecount -= 1
+
+    def p(self):
+        print(''.join(['-' for _ in range(56)]))
+
+        j = 0
+        for r in self.m[1:]:
+            i = 1
+            for v in r[1:]:
+                print(str(v).ljust(4), end=' | ' if i % 3 == 0 else ', ')
+                i += 1
+            j += 1
+            print()
+            if j % 3 == 0:
+                print(''.join(['-' for _ in range(56)]))
+
+
+import random
+
+random.seed(42)
+
+
+def solve_sudoku(b):
+    def next_square(b):
+        p = (random.randint(1, 9), random.randint(1, 9))
+        while b.m[p[0]][p[1]] is not None:
+            p = (random.randint(1, 9), random.randint(1, 9))
+        return p
+
+    def possible_next_moves(x, y, b):
+        available = set([i for i in range(1, 10)])
+
+        for i in range(1, 10):
+            if b.m[i][y] is not None and b.m[i][y] in available:
+                available.remove(b.m[i][y])
+
+        for j in range(1, 10):
+            if b.m[x][j] is not None and b.m[x][j] in available:
+                available.remove(b.m[x][j])
+
+        return list(available)
+
+    def is_a_solution(a, k, b):
+        return b.freecount == 0
+
+    def process_solution(a, k, b):
+        b.p()
+        return True
+
+    def construct_candidates(a, k, b):
+        x, y = next_square(b)
+
+        b.move[k] = Point(x, y)
+
+        next_move_candidates = possible_next_moves(x, y, b)
+
+        return next_move_candidates, len(next_move_candidates)
+
+    def make_move(a, k, b):
+        x, y = b.move[k]
+        b.m[x][y] = a[k]
+        b.freecount -= 1
+
+    def unmake_move(a, k, b):
+        x, y = b.move[k]
+        b.m[x][y] = None
+        b.freecount += 1
+
+    backtrack([None] * (9 ** 2 + 1), 0, b,
+              is_a_solution=is_a_solution,
+              process_solution=process_solution,
+              construct_candidates=construct_candidates,
+              make_move=make_move,
+              unmake_move=unmake_move
+              )
+
+
+b = Board([[1, 8, 1], [1, 9, 2], [2, 5, 3], [2, 6, 5],
+           [3, 4, 6], [3, 8, 7], [4, 1, 7], [4, 7, 3], [5, 4, 4], [5, 7, 8],
+           # [6, 1, 1], [7, 4, 1], [7, 5, 2], [8, 2, 8], [8, 8, 4], [9, 2, 5],
+           [9, 7, 6], [1, 1, 6], [2, 1, 9], [3, 1, 8], [5, 1, 5], [7, 1, 4],
+           # [1, 2, 7], [1, 3, 3], [1, 4, 8], [1, 5, 9], [1, 6, 4], [2, 2, 1],
+           [2, 3, 2], [2, 4, 7], [2, 7, 4], [2, 9, 6], [9, 9, 8], [9, 1, 3],
+           [9, 4, 9], [9, 5, 4], [6, 2, 3], [6, 3, 4], [6, 4, 5], [6, 5, 8],
+           # [6, 6, 9], [6, 7, 2], [6, 8, 6], [6, 9, 7], [4, 2, 9], [4, 3, 8],
+           [4, 4, 2], [4, 5, 6], [8, 1, 2], [8, 3, 7], [8, 4, 3], [8, 5, 5],
+           [8, 6, 6], [8, 7, 1], [8, 8, 4], [8, 9, 9], [7, 7, 7], [7, 8, 3],
+           [7, 9, 5], [4, 8, 5], [3, 2, 4], [3, 3, 5]],
+          )
+print(b.freecount)
+b.p()
+print(solve_sudoku(b))
